ea8313e354375f8f7e0d1745643739ca37a5a6d5
aarya-0-0 <pranishashrestha27@gmail.com>
2026-09-10 15:15:11 +0545

    Adding full-screen functionality

--- backend.py
+++ backend.py
@@ -81,9 +81,10 @@ def add_media():
 @app.route('/media/<folder_name>/<filename>')
 def serve_media(folder_name,filename):
     return send_from_directory(os.path.join('media',folder_name,),filename)
-@app.route('/delete-media/<int:media_id', methods='POST')
+
+@app.route('/delete-media/<int:media_id>', methods=['POST'])
 def delete_media(media_id):
-    cursor.execute("select folder_id and media_name from media where media_id=%s",(media_id,))
+    cursor.execute("select folder_id , media_name from media where media_id=%s",(media_id,))
     media=cursor.fetchone()
     if media:
         folder_id=media[0]
@@ -95,7 +96,7 @@ def delete_media(media_id):
         file_path=os.path.join('media',folder_name,media_name)
         if os.path.exists(file_path):
             os.remove(file_path)
-            
+
         cursor.execute("Delete from media where media_id=%s",(media_id,))
         db.commit()
         return redirect(f'/media-page/{folder_id}')
